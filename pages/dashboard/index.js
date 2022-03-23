5a1aadcf7f1be40e64e87656545e34d8af8fece2
pritam-digichal <pritam.singh@digichal.in>
2022-03-23 12:53:28 +0530

    image added

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,19 @@
-export default function Dashboard() {
-  return <h1>Home</h1>;
+import DashboardDesktop from "./desktop";
+import DasboardMobile from "./mobile";
+import { useMediaQuery } from "react-responsive";
+
+function HomePage() {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const isDesktopOrLaptop = useMediaQuery({
+    query: "(min-width: 1224px)",
+  });
+
+  return (
+    <>
+      {isDesktopOrLaptop && <DashboardDesktop />}
+      {isTabletOrMobile && <DasboardMobile />}
+    </>
+  );
 }
+
+export default HomePage;
